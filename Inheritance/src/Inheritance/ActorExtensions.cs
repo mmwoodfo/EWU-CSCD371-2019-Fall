96165911ffc7adda8b1a411e7b7cfb8459c5adcb
Meg <mmwoodfo@asu.edu>
2019-10-16 14:34:43 -0700

    Adding unimplemented extension method speak to new class

---
+++ Inheritance/src/Inheritance/ActorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public static class ActorExtensions
+    {
+        public static void Speak(this Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
