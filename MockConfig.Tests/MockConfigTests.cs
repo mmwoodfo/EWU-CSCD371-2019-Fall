0f722287496871769963e7b4cc10af0bf2ba84f7
Meg <mmwoodfo@asu.edu>
2019-10-23 22:48:11 -0700

    Creating mock config unit tests & implementing solution

---
+++ MockConfig.Tests/MockConfigTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockConfig;
+
+namespace MockConfig.Tests
+{
+    [TestClass()]
+    public class MockConfigTests
+    {
+        [TestMethod]
+        public void MockConfig_GetConfigValue_ReturnsFalse()
+        {
+            //arrange
+            string name = "Wrong Path Name", value = "TESTVALUE";
+            MockConfig testSystem = new MockConfig();
+
+            //act
+            bool tf = testSystem.GetConfigValue(name, value);
+
+            //assert
+            Assert.IsFalse(tf);
+        }
+
+        [DataTestMethod]
+        [DataRow("NAME1", "TESTVALUE")]
+        [DataRow("NAME2", "TESTVALUE")]
+        [DataRow("NAME3", "TESTVALUE")]
+        [DataRow("NAME4", "TESTVALUE")]
+        public void MockConfig_GetConfigValue_ReturnsTrue(string name, string value)
+        {
+            //arrange
+            MockConfig testSystem = new MockConfig();
+
+            //act
+            bool tf = testSystem.GetConfigValue(name, value);
+
+            //assert
+            Assert.IsTrue(tf);
+        }
+
+        [DataTestMethod]
+        [DataRow("NAME5", "Not Valid")]
+        [DataRow("NAME5", null)]
+        [DataRow("NAME5", "NOT=VALID")]
+        [DataRow("NAME5", "")]
+        public void MockConfig_SetConfigValue_ReturnsFalse(string name, string value)
+        {
+            //arrange
+            MockConfig testSystem = new MockConfig();
+
+            //act
+            bool tf = testSystem.SetConfigValue(name, value);
+
+            //assert
+            Assert.IsFalse(tf);
+        }
+
+        [DataTestMethod]
+        [DataRow("NAME5", "VALUE5")]
+        [DataRow("NAME6", "VALUE6")]
+        [DataRow("NAME7", "VALUE7")]
+        [TestMethod]
+        public void MockConfig_SetConfigValue_ReturnsTrue(string name, string value)
+        {
+            //arrange
+            MockConfig testSystem = new MockConfig();
+
+            //act
+            bool tf = testSystem.SetConfigValue(name, value);
+
+            //assert
+            Assert.IsTrue(tf);
+        }
+    }
+}
---
+++ MockConfig/MockConfig.cs
@@ -0,0 +1,50 @@
+﻿using Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MockConfig
+{
+    public class MockConfig : IConfig
+    {
+
+        private List<Tuple<string, string?>> configList;
+
+        public MockConfig()
+        {
+            configList = new List<Tuple<string, string?>>();
+
+            configList.Add(new Tuple<string, string?>("NAME1", "VALUE1"));
+            configList.Add(new Tuple<string, string?>("NAME2", "VALUE2"));
+            configList.Add(new Tuple<string, string?>("NAME3", "VALUE3"));
+            configList.Add(new Tuple<string, string?>("NAME4", "VALUE4"));
+        }
+
+        public bool GetConfigValue(string name, string? value)
+        {
+            foreach (Tuple<string, string?> item in configList)
+            {
+                if (item.Item1.Contains(name))
+                {
+                    value = item.Item2;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if (value is null || value.Contains(' ') || value.Contains('=') || value.Equals(""))
+            {
+                return false;
+            }
+            else
+            {
+                Tuple<string, string?> configItem = new Tuple<string, string?>(name, value);
+                configList.Add(configItem);
+                return true;
+            }
+        }
+    }
+}
