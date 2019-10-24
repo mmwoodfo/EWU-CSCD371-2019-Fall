94e546337c2769020938e5442d6795c8ee5c1a6d
Meg <mmwoodfo@asu.edu>
2019-10-23 19:04:52 -0700

    Setting up Environment Config class and unit tests

---
+++ Configuration.Tests/EnvironmentConfigTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Configuration.Tests
+{
+    [TestClass()]
+    public class EnvironmentConfigTests
+    {
+        [TestMethod]
+
+        public void EnvironmentConfig_GetConfigValue_ReturnsFalse()
+        {
+            //arrange
+            string name = "Wrong Path Name", value = "TESTVALUE";
+            EnvironmentConfig testSystem = new EnvironmentConfig();
+
+            //act
+            bool tf = testSystem.GetConfigValue(name, value);
+
+            //assert
+            Assert.IsFalse(tf);
+        }
+
+        [TestMethod]
+        public void EnvironmentConfig_GetConfigValue_ReturnsTrue()
+        {
+            //arrange
+            string name = "TEMP", value = "TESTVALUE";
+            EnvironmentConfig testSystem = new EnvironmentConfig();
+
+            //act
+            bool tf = testSystem.GetConfigValue(name, value);
+
+            //assert
+            Assert.IsTrue(tf);
+        }
+
+        [TestMethod]
+        public void EnvironmentConfig_SetConfigValue_ReturnsFalse()
+        {
+            //arrange
+            string name = "Wrong Path Name";
+            string? value = null;
+            EnvironmentConfig testSystem = new EnvironmentConfig();
+
+            //act
+            bool tf = testSystem.SetConfigValue(name, value);
+
+            //assert
+            Assert.IsFalse(tf);
+        }
+
+        [TestMethod]
+        public void EnvironmentConfig_SetConfigValue_ReturnsTrue()
+        {
+            //arrange
+            string name = "TEMP", value = "TESTVALUE";
+            EnvironmentConfig testSystem = new EnvironmentConfig();
+
+            //act
+            bool tf = testSystem.SetConfigValue(name, value);
+
+            //assert
+            Assert.IsTrue(tf);
+        }
+    }
+}
---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+
+namespace Configuration
+{
+    public sealed class EnvironmentConfig : IConfig
+    {
+        public bool GetConfigValue(string name, string? value)
+        {
+            if(Environment.GetEnvironmentVariable(name) is null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+            else
+            {
+                Environment.SetEnvironmentVariable(name, value);
+                return true;
+            }
+        }
+
+        //~EnvironmentConfig()
+        //{
+        //    //some kind of cleanup?
+        //}
+    }
+}
