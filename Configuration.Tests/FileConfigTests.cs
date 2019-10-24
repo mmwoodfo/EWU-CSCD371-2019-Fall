0582ab70994e0da259a55cdbaa9d3483b7366f70
Meg <mmwoodfo@asu.edu>
2019-10-23 22:47:15 -0700

    Adding/fixing unit tests - implementing file config to pass unit tests

---
+++ Configuration.Tests/FileConfigTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Configuration.Tests
+{
+    [TestClass()]
+    public class FileConfigTests
+    {
+        [TestMethod()]
+        public void FileConfig_ReadConfig()
+        {
+            //Arrange
+            string filePath = Path.GetRandomFileName();
+            List<Tuple<string, string?>> expectedConfigList = new List<Tuple<string, string?>>();
+            expectedConfigList.Add(new Tuple<string, string?>("NAME1", "VALUE1"));
+            expectedConfigList.Add(new Tuple<string, string?>("NAME2", "VALUE2"));
+            expectedConfigList.Add(new Tuple<string, string?>("NAME3", "VALUE3"));
+
+            try
+            {
+                GenerateTestFile(filePath);
+                FileConfig fileConfig = new FileConfig(filePath);
+
+                //Act
+                var configList = fileConfig.ReadConfig();
+
+                //Assert
+                CollectionAssert.AreEqual(expectedConfigList, configList);
+                CollectionAssert.AreEqual(expectedConfigList, configList);
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [TestMethod()]
+        public void FileConfig_WriteConfig()
+        {
+            //Arrange
+            string filePath = Path.GetRandomFileName();
+
+            try
+            {
+                FileConfig fileConfig = new FileConfig(filePath);
+
+                //Act
+                fileConfig.WriteConfig("NAME1", "VALUE1");
+                fileConfig.WriteConfig("NAME2", "VALUE2");
+                string[] fileLines = File.ReadLines(filePath).ToArray();
+
+                //Assert
+                Assert.AreEqual(fileLines.Length, 2);
+                Assert.AreEqual(fileLines[0], "NAME1=VALUE1");
+                Assert.AreEqual(fileLines[1], "NAME2=VALUE2");
+            }
+            finally
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        [TestMethod()]
+        public void FileConfig_ValidateNameAndValue_ValueFail()
+        {
+            //Arrange
+            string name = "PATHTEST", value = "VALUE=value";
+            FileConfig fileConfig = new FileConfig("unnecessary");
+
+            //Act
+            bool tf = fileConfig.ValidateNameAndValue(name, value);
+
+            //Assert
+            Assert.IsFalse(tf);
+        }
+
+        [TestMethod()]
+        public void FileConfig_ValidateNameAndValue_VaLuePass()
+        {
+            //Arrange
+            string name = "PATHTEST", value = "value";
+            FileConfig fileConfig = new FileConfig("unnecessary");
+
+            //Act
+            bool tf = fileConfig.ValidateNameAndValue(name, value);
+
+            //Assert
+            Assert.IsTrue(tf);
+        }
+
+        [TestMethod()]
+        public void FileConfig_ValidateNameAndValue_NameFail()
+        {
+            //Arrange
+            string name = "TEMP PATH", value = "VALUE";
+            FileConfig fileConfig = new FileConfig("unnecessary");
+
+            //Act
+            bool tf = fileConfig.ValidateNameAndValue(name, value);
+
+            //Assert
+            Assert.IsFalse(tf);
+        }
+
+        [TestMethod()]
+        public void FileConfig_ValidateNameAndValue_NamePass()
+        {
+            //Arrange
+            string name = "path", value = "VALUE";
+            FileConfig fileConfig = new FileConfig("unnecessary");
+
+            //Act
+            bool tf = fileConfig.ValidateNameAndValue(name, value);
+
+            //Assert
+            Assert.IsTrue(tf);
+        }
+
+        private void GenerateTestFile(string filePath)
+        {
+            using(StreamWriter sw = new StreamWriter(filePath, true))
+            {
+                sw.WriteLine("NAME1=VALUE1");
+                sw.WriteLine("NAME2=VALUE2");
+                sw.WriteLine("NAME3=VALUE3");
+            }
+        }
+    }
+}
--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Configuration;
+using System;
+using System.Collections.Generic;
 
 namespace SampleApp
 {
@@ -6,7 +8,22 @@ namespace SampleApp
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            FileConfig fileConfig = new FileConfig("Config.settings");
+
+            fileConfig.WriteConfig("PATH1","VALUE1");
+            fileConfig.WriteConfig("PATH2", "VALUE2");
+            fileConfig.WriteConfig("PATH3", "VALUE3");
+            fileConfig.WriteConfig("PATH4", "VALUE4");
+            fileConfig.WriteConfig("PATH5", "VALUE5");
+
+            List<Tuple<string, string?>> configList = fileConfig.ReadConfig();
+
+            for(int i = 0; i < configList.Count; i++)
+            {
+                Console.WriteLine(configList[i].Item1 + " = " + configList[i].Item2);
+            }
+
+            System.IO.File.WriteAllText("Config.settings", string.Empty);
         }
     }
 }
