b6a011038d4e990c0776cfd1167b1f2272b92868
Meg <mmwoodfo@asu.edu>
2019-11-05 22:13:58 -0700

    Setting up unit tests & auto implementation of ICollection

--- Assignment6/Array.cs
+++ Assignment6/Array.cs
@@ -1,10 +1,57 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Assignment6
 {
-    public class Array<T>
+    public class Array<T> : ICollection<T>
     {
-        
+        public T[] Items { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => ((ICollection<T>)Items).Count;
+
+        public bool IsReadOnly => ((ICollection<T>)Items).IsReadOnly;
+
+        public Array(int width)
+        {
+            Capacity = width;
+            Items = new T[Capacity];
+        }
+
+        public void Add(T item)
+        {
+            ((ICollection<T>)Items).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<T>)Items).Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return ((ICollection<T>)Items).Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ((ICollection<T>)Items).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return ((ICollection<T>)Items).Remove(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((ICollection<T>)Items).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((ICollection<T>)Items).GetEnumerator();
+        }
     }
 }
---
+++ Assignment6Tests/ArrayTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment6;
+using System;
+
+namespace Assignment6.Tests
+{
+    [TestClass()]
+    public class ArrayTests
+    {
+        [TestMethod()]
+        public void ArrayTest()
+        {
+            //Arrange
+            Array<int> items = new Array<int>(10);
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void ContainsTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void CopyToTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void GetEnumeratorTest()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+    }
+}
