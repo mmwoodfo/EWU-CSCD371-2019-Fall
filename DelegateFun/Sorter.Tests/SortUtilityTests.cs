82a8d4a4381f81554b49fe87065b02c15514368a
Meg <mmwoodfo@asu.edu>
2019-11-14 22:36:18 -0700

    Adding initial tests & logic to project

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Sorter.Tests
 {
@@ -6,8 +7,68 @@ namespace Sorter.Tests
     public class SortUtilityTests
     {
         [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        public void QuickSort_NullArray_ThrowNullException()
         {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(()=> sorter.QuickSort(null!, ((int num1, int num2) => num1 < num2), 0, 0));
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 1, 2, 3, 3, 4, 10, 15, 32, 302 })]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        [TestMethod]
+        public void QuickSort_UsingAnonymousMethod_SortsAscending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+            Compare compareNumbers = delegate (int num1, int num2)
+            {
+                return num1 < num2;
+            };
+
+            //Act
+            sorter.QuickSort(intArray, compareNumbers, 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 1, 2, 3, 3, 4, 10, 15, 32, 302 })]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        [TestMethod]
+        public void QuickSort_UsingLambdaExpression_SortsAscending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+
+            //Act
+            sorter.QuickSort(intArray, ((int num1, int num2) => num1 < num2), 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 1, 2, 3, 3, 4, 10, 15, 32, 302 })]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        [TestMethod]
+        public void QuickSort_UsingLambdaStatement_SortsAscending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+
+            //Act
+            sorter.QuickSort(intArray, ((int num1, int num2) => { return num1 < num2; }), 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -2,9 +2,49 @@
 
 namespace Sorter
 {
+    public delegate bool Compare(int first, int second);
+
     public class SortUtility
     {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+        private int QuickSortPartition(int[] intArray, Compare comparer, int low, int high)
+        {
+            int pivot = intArray[high],
+                        smaller = low - 1;
+
+            for(int larger = low; larger < high; larger++)
+            {
+                if(comparer(intArray[larger], pivot))
+                {
+                    smaller++;
+
+                    int temp1 = intArray[smaller];
+                    intArray[smaller] = intArray[larger];
+                    intArray[larger] = temp1;
+                }
+            }
+
+            int temp2 = intArray[smaller + 1];
+            intArray[smaller + 1] = intArray[high];
+            intArray[high] = temp2;
+
+            return smaller + 1;
+        }
+
+        public void QuickSort(int[] intArray, Compare comparer, int low, int high)
+        {
+            if(intArray is null)
+            {
+                throw new ArgumentNullException(nameof(intArray));
+            }
+
+            if(low < high)
+            {
+                int partitionIndex = QuickSortPartition(intArray, comparer, low, high);
+
+                QuickSort(intArray, comparer, low, partitionIndex-1);
+                QuickSort(intArray, comparer, partitionIndex+1, high);
+            }
+        }
+
     }
 }
