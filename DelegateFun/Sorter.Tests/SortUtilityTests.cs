5c3e49749ed7ef4362e6dd8b239eca42946b09b7
Meg <mmwoodfo@asu.edu>
2019-11-15 18:31:11 -0700

    Adding descending sort test

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -70,5 +70,60 @@ namespace Sorter.Tests
             //Assert
             CollectionAssert.AreEqual(expectedIntArray, intArray);
         }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 302, 32, 15, 10, 4, 3, 3, 2, 1 })]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 3, 2, 1 })]
+        [TestMethod]
+        public void QuickSort_UsingAnonymousMethod_SortsDescending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+            Compare compareNumbers = delegate (int num1, int num2)
+            {
+                return num1 > num2;
+            };
+
+            //Act
+            sorter.QuickSort(intArray, compareNumbers, 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 302, 32, 15, 10, 4, 3, 3, 2, 1 })]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 3, 2, 1 })]
+        [TestMethod]
+        public void QuickSort_UsingLambdaExpression_SortsDescending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+
+            //Act
+            sorter.QuickSort(intArray, ((int num1, int num2) => num1 > num2), 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, new int[] { 3, 2, 1 })]
+        [DataRow(new int[] { 2, 1, 3, 10, 4, 32, 302, 3, 15 }, new int[] { 302, 32, 15, 10, 4, 3, 3, 2, 1 })]
+        [DataRow(new int[] { 3, 2, 1 }, new int[] { 3, 2, 1 })]
+        [TestMethod]
+        public void QuickSort_UsingLambdaStatement_SortsDesending(int[] intArray, int[] expectedIntArray)
+        {
+            //Arrange
+            SortUtility sorter = new SortUtility();
+
+            //Act
+            sorter.QuickSort(intArray, ((int num1, int num2) => { return num1 > num2; }), 0, intArray.Length - 1);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedIntArray, intArray);
+        }
     }
 }
