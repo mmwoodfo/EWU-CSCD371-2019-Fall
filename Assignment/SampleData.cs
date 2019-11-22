c0bb6d28dfb0838db60f7cbfb5c546c739d561e6
Meg <mmwoodfo@asu.edu>
2019-11-21 18:02:27 -0700

    Setting up project & implementing CsvRows

--- Assignment/SampleData.cs
+++ Assignment/SampleData.cs
@@ -1,30 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
-        // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        private string _FilePath;
 
-        // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public SampleData(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException(nameof(filePath));
+            _FilePath = filePath;
+        }
 
-        // 3.
+        public IEnumerable<string> CsvRows {
+            get {
+                string[] lines = File.ReadAllLines(_FilePath);
+                lines = lines.Skip(1).ToArray();
+
+                foreach (String line in lines)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+        public IEnumerable<IPerson> People { get; }
+
+        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
+        {
+            throw new NotImplementedException();
+        }
+
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+        {
+            throw new NotImplementedException();
+        }
 
-        // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
-
-        // 5.
-        public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
-
-        // 6.
-        public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ AssignmentTests/SampleDataTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace Assignment.Tests
+{
+    [TestClass()]
+    public class SampleDataTests
+    {
+        string _FilePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName + @"\Assignment" + @"\" + "People.csv";
+
+
+        [TestMethod()]
+        public void SampleData()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void CsvRows()
+        {
+            /*I'll be changing this - just trying to figure out the best method of testing with csv files*/
+            //Arrange
+            SampleData sampleData = new SampleData(_FilePath);
+
+            //Act
+            IEnumerable<string> lines = sampleData.CsvRows;
+
+            //Assert
+            Assert.IsTrue(lines.Contains("1,Priscilla,Jenyns,pjenyns0@state.gov,7884 Corry Way,Helena,MT,70577"));
+        }
+
+        [TestMethod()]
+        public void FilterByEmailAddress()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void GetAggregateListOfStatesGivenPeopleCollection()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void GetAggregateSortedListOfStatesUsingCsvRows()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+        }
+    }
+}
