056bb56244898d4619b90a29dd9ee3d9d3909a86
Meg <mmwoodfo@asu.edu>
2019-11-24 16:25:31 -0700

    Adding person & address tests

---
+++ AssignmentTests/AddressTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Tests
+{
+    [TestClass()]
+    public class AddressTests
+    {
+        [TestMethod()]
+        public void Address_ValidData_Succuss()
+        {
+            //Arrange
+            string street = "123 somewhere street", city = "Glendale", state = "AZ", zip = "54321";
+
+            //Act
+            Address address = new Address(street, city, state, zip);
+
+            //Assert
+            try
+            {
+                Assert.AreEqual(street, address.StreetAddress);
+                Assert.AreEqual(city, address.City);
+                Assert.AreEqual(state, address.State);
+                Assert.AreEqual(zip, address.Zip);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [TestMethod()]
+        public void Address_NullStreet_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Address(null!, "city", "state", "zip"));
+        }
+
+        [TestMethod()]
+        public void Address_NullCity_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Address("street", null!, "state", "zip"));
+        }
+
+        [TestMethod()]
+        public void Address_NullState_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Address("street", "city", null!, "zip"));
+        }
+
+        [TestMethod()]
+        public void Address_NullZip_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Address("street", "city", "state", null!));
+        }
+    }
+}
---
+++ AssignmentTests/PersonTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment.Tests
+{
+    [TestClass()]
+    public class PersonTests
+    {
+        [TestMethod()]
+        public void Person_ValidData_Succuss()
+        {
+            //Arrange
+            string first = "123 somewhere street", last = "Glendale", email = "AZ";
+
+            //Act
+            Person person = new Person(first, last, email, new Address("street", "city", "state", "zip"));
+
+            //Assert
+            try
+            {
+                Assert.AreEqual(first, person.FirstName);
+                Assert.AreEqual(last, person.LastName);
+                Assert.AreEqual(email, person.EmailAddress);
+                Assert.AreEqual("street", person.Address.StreetAddress);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [TestMethod()]
+        public void Person_NullFirst_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Person(null!, "Last", "Email@email.com", new Address("street","city","state","zip")));
+        }
+
+        [TestMethod()]
+        public void Person_NullLast_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Person("First", null!, "Email@email.com", new Address("street", "city", "state", "zip")));
+        }
+
+        [TestMethod()]
+        public void Person_NullEmail_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Person("First", "Last", null!, new Address("street", "city", "state", "zip")));
+        }
+
+        [TestMethod()]
+        public void Person_NullAddress_ThrowsArgumentNullException()
+        {
+            //Arrange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new Person("First", "Last", "Email@email.com", null!));
+        }
+    }
+}
