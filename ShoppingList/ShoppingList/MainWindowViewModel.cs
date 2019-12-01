b7d8d3fa9d01719a7b60d556c197c8aa388bb19f
Meg <mmwoodfo@asu.edu>
2019-12-01 16:02:38 -0700

    Adding color bindings & setting up textbox property binding

--- ShoppingList/ShoppingList/MainWindowViewModel.cs
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -6,20 +6,35 @@ namespace ShoppingList
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
 
+        //--------- STYLE BINDINGS --------------//
+        public string Margins { get; }
+        public string Tomato { get; }
+        public string Sandy { get; }
+        public string SlateGray { get; }
+
+        //--------- OTHER BINDINGS --------------//
         public ObservableCollection<Item> ShoppingList { get; } = new ObservableCollection<Item>();
+        public string TextToAddToList { get; set; } = "";
         public ICommand AddItemCommand { get; }
 
         public MainWindowViewModel()
         {
+            Margins = "10,10,10,10";
+            Tomato = "#FFF65158";
+            Sandy = "#FFFBDE44"; 
+            SlateGray = "#FF28324B"; 
+
             AddItemCommand = new Command(OnAddItem);
         }
 
         private void OnAddItem()
         {
-            ShoppingList.Add(new Item($"Apples {ShoppingList.Count}", 3));
+            ShoppingList.Add(new Item($"{TextToAddToList}", 3));
+            TextToAddToList = "";
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShoppingList)));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextToAddToList)));
         }
     }
 }
