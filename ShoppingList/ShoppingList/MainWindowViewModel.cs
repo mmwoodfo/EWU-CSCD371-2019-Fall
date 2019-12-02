fda8ead8e2b1677aee3ccab13411108f0f2a31a8
Meg <mmwoodfo@asu.edu>
2019-12-01 17:20:49 -0700

    Adding item editing interface & simplifying ShoppingList property

--- ShoppingList/ShoppingList/MainWindowViewModel.cs
+++ ShoppingList/ShoppingList/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 
@@ -9,32 +10,37 @@ namespace ShoppingList
         public event PropertyChangedEventHandler? PropertyChanged;
 
         //--------- STYLE BINDINGS --------------//
-        public string Margins { get; }
-        public string Tomato { get; }
-        public string Sandy { get; }
-        public string SlateGray { get; }
+        public string Margins { get; } = "10,10,10,10";
+        public string Tomato { get; } = "#FFF65158";
+        public string Sandy { get; } = "#FFFBDE44";
+        public string SlateGray { get; } = "#FF28324B";
+        public short SpanAll { get; } = Int16.MaxValue; //32767
 
         //--------- OTHER BINDINGS --------------//
-        public ObservableCollection<Item> ShoppingList { get; } = new ObservableCollection<Item>();
+        public ObservableCollection<string> ShoppingList { get; } = new ObservableCollection<string>();
         public string TextToAddToList { get; set; } = "";
+        public string SelectedItem { get; set; } = "";
         public ICommand AddItemCommand { get; }
+        public string DeleteBtnVisiblity { get; set; } = "Hidden";
+        public string UpdateBtnVisibility { get; set; } = "Hidden";
+        public string UpdateTxtVisibility { get; set; } = "Hidden";
 
+        //------------ CONSTRUCTOR -------------//
         public MainWindowViewModel()
         {
-            Margins = "10,10,10,10";
-            Tomato = "#FFF65158";
-            Sandy = "#FFFBDE44"; 
-            SlateGray = "#FF28324B"; 
-
             AddItemCommand = new Command(OnAddItem);
         }
 
+        //---------- BINDING COMMANDS ------------//
         private void OnAddItem()
         {
-            ShoppingList.Add(new Item($"{TextToAddToList}", 3));
-            TextToAddToList = "";
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShoppingList)));
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextToAddToList)));
+            if (!string.IsNullOrEmpty(TextToAddToList))
+            {
+                ShoppingList.Add($"• {TextToAddToList}");
+                TextToAddToList = "";
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShoppingList)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextToAddToList)));
+            }
         }
     }
 }
