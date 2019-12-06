663b706fdddc07bbf4a8a1820dc2e666b8dd06a3
Meg <mmwoodfo@asu.edu>
2019-12-06 16:12:15 -0700

    Doing some unit tests

---
+++ ShoppingList.Tests/ItemStrikeThroughConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows;
+
+namespace ShoppingList.Tests
+{
+    [TestClass()]
+    public class ItemStrikeThroughConverterTests
+    {
+        [TestMethod()]
+        public void Convert_GivenTrue_ReturnStrikeThrough()
+        {
+            //Arrange
+            ItemStrikeThroughConverter visibilityConverter = new ItemStrikeThroughConverter();
+            Item item = new Item("Apples")
+            {
+                CheckedOff = true
+            };
+
+            var visibility = visibilityConverter.Convert(item.CheckedOff);
+
+            Assert.AreEqual(TextDecorations.Strikethrough, visibility);
+        }
+
+        [TestMethod()]
+        public void Convert_GivenFalse_ReturnNull()
+        {
+            //Arrange
+            ItemStrikeThroughConverter visibilityConverter = new ItemStrikeThroughConverter();
+            Item item = new Item("Apples")
+            {
+                CheckedOff = false
+            };
+
+            var visibility = visibilityConverter.Convert(item.CheckedOff);
+
+            Assert.AreEqual(null, visibility);
+        }
+    }
+}
