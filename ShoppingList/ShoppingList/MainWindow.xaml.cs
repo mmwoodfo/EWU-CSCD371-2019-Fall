b421bbbcf7b267e6a250c7571824149841600dc9
Meg <mmwoodfo@asu.edu>
2019-12-07 18:05:43 -0700

    Fixing keyboard focus method in code behind

--- ShoppingList/ShoppingList/MainWindow.xaml.cs
+++ ShoppingList/ShoppingList/MainWindow.xaml.cs
@@ -8,16 +8,18 @@ namespace ShoppingList
     public partial class MainWindow : Window
     {
         //public MainWindowViewModel ViewModel { get; }
+        public ViewModelLocator ViewModel { get; }
 
         public MainWindow()
         {
+            ViewModel = new ViewModelLocator();
             //DataContext = ViewModel = new MainWindowViewModel(_DialogService);
             InitializeComponent();
         }
 
         private void TextBox_OnGotKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
         {
-            //    ViewModel.SelectedItem = null;
+            ViewModel.MainWindow.SelectedItem = null;
         }
     }
 }
